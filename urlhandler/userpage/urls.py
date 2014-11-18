e406f69018c8d8e192a2161c633ac3ac8e7eac76
Smoudy <bogeliu@126.com>
2014-11-19 01:33:21 +0800

    调试失败还原ticket界面
    估计要重新运行服务器才行，等本地服务配置了

--- urlhandler/userpage/urls.py
+++ urlhandler/userpage/urls.py
@@ -12,6 +12,6 @@ urlpatterns = patterns('',
                        url(r'^helplecture/$','userpage.views.helplecture_view'),
                        url(r'^activity/(?P<actid>\d+)/menu/$', 'userpage.views.activity_menu_view'),
 					   #以下为选座的url--刘博格,刘峻琳
-					   url(r'^ticket/mainseat/(?P<UID>\S+)/$','userpage.views.choose_seat_mainmenu',name='mainmenu'),
+					   url(r'^ticket/mainseat/(?P<uid>\S+)/$','userpage.views.choose_seat_mainmenu',name='mainmenu'),
 					   url(r'^ticket/subseat/(?P<uid>\S+)/(?P<block_id>\d+)/$','userpage.views.choose_seat_submenu',name='submenu'),
                        )
