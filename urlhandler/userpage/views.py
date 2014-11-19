dba9a5dce677301a5647b9408a4555e747ea9a54
Smoudy <bogeliu@126.com>
2014-11-19 19:53:02 +0800

    debug

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -197,8 +197,7 @@ def ticket_view(request, uid):
     variables=RequestContext(request,{'act_id':act_id, 'act_name':act_name,'act_place':act_place, 'act_begintime':act_begintime,
                                       'act_endtime':act_endtime,'act_photo':act_photo, 'ticket_status':ticket_status,
                                       'ticket_seat':ticket_seat,
-                                      'act_key':act_key,
-									  'uid':uid})
+                                      'act_key':act_key,'uid':uid})
     return render_to_response('activityticket.html', variables)
 
 def help_view(request):
