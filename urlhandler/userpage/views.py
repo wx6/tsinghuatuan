cfb4896ea3f06f03520e84638f0a956cffde276b
Smoudy <bogeliu@126.com>
2014-11-21 19:57:12 +0800

    debug

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -199,7 +199,7 @@ def ticket_view(request, uid):
     act_photo = "http://qr.ssast.org/fit/"+uid
     #act_photo = get_2D_barcodes(ticket[0].barcode_key)
     mainmenu = s_safe_reverse_seat_mainmenu(uid)
-    variables=RequestContext(request,{'mainmenu':mainmenu,'uid':uid,'act_id':act_id, 'act_name':act_name,'act_place':act_place, 'act_begintime':act_begintime,
+    variables=RequestContext(request,{'abd':2, 'mainmenu':mainmenu,'uid':uid,'act_id':act_id, 'act_name':act_name,'act_place':act_place, 'act_begintime':act_begintime,
                                       'act_endtime':act_endtime,'act_photo':act_photo, 'ticket_status':ticket_status,
                                       'ticket_seat':ticket_seat,
                                       'act_key':act_key})
