c286db4dcfe10e6ff9e762d9a823aa57baa93172
thssljl2012 <frankliujunlin@163.com>
2014-11-22 15:57:10 +0800

    debug

--- urlhandler/userpage/urls.py
+++ urlhandler/userpage/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
                        url(r'^helpclub/$','userpage.views.helpclub_view'),
                        url(r'^helplecture/$','userpage.views.helplecture_view'),
                        url(r'^activity/(?P<actid>\d+)/menu/$','userpage.views.activity_menu_view'),
-                       url(r'^ticket/mainseat/(?P<uid>\S+)/$','userpage.views.seat_mainmenu'),
+                       # url(r'^ticket/mainseat/(?P<uid>\S+)/$','userpage.views.seat_mainmenu'),
+                       url(r'^ticket/mainseat/$','userpage.views.seat_mainmenu_view'),
                        url(r'^ticket/subseat/(?P<uid>\S+)/(?P<block_id>\d+)/$','userpage.views.seat_submenu'),
                        )
--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -249,7 +249,7 @@ def helplecture_view(request):
 
 #一下为选座view部分，尚未添加处理--刘博格,刘峻琳
 
-def seat_mainmenu(request, uid):
+def seat_mainmenu_view(request):
     print 'aaaaaaaaaaaaaaaaaaaaaa'
     # variables=RequestContext(request,{'uid':uid})
     return render_to_response('seat_mainmenu.html', {
