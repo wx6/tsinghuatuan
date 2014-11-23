a6833ea386230e42d3d0707290ab6e1c5ffebfad
Smoudy <bogeliu@126.com>
2014-11-23 14:26:53 +0800

    debug

--- urlhandler/userpage/safe_reverse.py
+++ urlhandler/userpage/safe_reverse.py
@@ -10,9 +10,12 @@ def s_reverse_activity_detail(activityid):
     return SITE_DOMAIN + reverse('userpage.views.details_view', kwargs={'activityid': activityid})
 
 
-def s_reverse_ticket_detail(uid):
-    return SITE_DOMAIN + reverse('userpage.views.ticket_view', kwargs={'uid': uid})
+#def s_reverse_ticket_detail(uid):
+#    return SITE_DOMAIN + reverse('userpage.views.ticket_view', kwargs={'uid': uid})
 
+def s_reverse_ticket_detail(uid):
+	print 'ccccccccccccccccccc'
+    return SITE_DOMAIN + reverse('userpage.views.seat_mainmenu_view')
 
 def s_reverse_help():
     return SITE_DOMAIN + reverse('userpage.views.help_view')
