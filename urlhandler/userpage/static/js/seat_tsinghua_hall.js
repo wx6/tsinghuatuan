002f2a7dd219a511427ed599f9b20d4669ddcfed
pengyou <a623917@126.com>
2014-11-24 17:06:36 +0800

    选座页面测试

---
+++ urlhandler/userpage/static/js/seat_tsinghua_hall.js
@@ -0,0 +1,36 @@
+function setTableId(){
+	var tds = document.getElementsByTagName("td")
+	var i = 0
+	for (i = 0; i < tds.length; i++){
+		tds[i].id = i
+	}
+}
+
+function changeState(id){
+	var td = document.getElementById(id)
+	if(td.className == 1){
+		if(lastid != -1){
+		var ltd = document.getElementById(lastid)
+		ltd.className = 1
+		ltd.innerHTML = "<span></span><img src="{% static 'img/可选座位.png' %}" style='width:100px;height:85px;'/>"
+		}
+		td.className = 3
+		td.innerHTML = "<span></span><img src="{% static 'img/选择座位.png' %}" style='width:100px;height:85px;'/>"
+		lastid = id
+	}
+	changeRecord(id)
+}
+
+function changeRecord(id){
+	row = parseInt(1 + id / length)
+	column = 1 + id % length 
+}
+
+setTableId()
+var length = 10
+var row = 0, column = 0
+var lastid = -1 
+$(".1").append("<img src="{% static 'img/可选座位.png' %}" style='width:100px;height:85px;'/>")
+$(".2").append("<img src="{% static 'img/不可选座位.png' %}" style='width:100px;height:85px;'/>")
+$(".3").append("<img src="{% static 'img/选择座位.png' %}" style='width:100px;height:85px;'/>")
+$(".4").append("<img src="{% static 'img/上次选择座位.png' %}" style='width:100px;height:85px;'/>")
