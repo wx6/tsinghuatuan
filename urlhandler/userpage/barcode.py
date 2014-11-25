969fe0d6c15c2aa18d3784e4bee9de5968fca175
李映辉 <thu2012lyh@126.com>
2014-11-25 22:15:40 +0800

    增加barcode文件中对其他模块的引入

--- urlhandler/userpage/barcode.py
+++ urlhandler/userpage/barcode.py
@@ -1,4 +1,10 @@
-def generate_2D_barcodes(key):
+from django.http import HttpResponse, Http404
+from django.template import RequestContext
+from weixinlib.settings import WEIXIN_TOKEN
+import urllib, urllib2
+
+
+def generate_2D_barcodes(request, key):
 	req_url =  'https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=' + WEIXIN_TOKEN
 	values = {"action_name":"QR_LIMIT_SCENE","action_info": {"scene": {"scene_id": key}}}
 	req_data = urllib.urlencode(values)
