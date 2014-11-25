676345fb2e4d5bde7fa610488e3416b6c2740519
李映辉 <thu2012lyh@126.com>
2014-11-26 00:19:00 +0800

    查找微信二维码问题

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -32,7 +32,7 @@ def get_timestamp():
 # generate two-dimensional barcodes
 def generate_2D_barcodes(key):
     req_url =  'https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=' + get_access_token()
-    values = {"action_name":"QR_LIMIT_SCENE","action_info": {"scene": {"scene_id": key}}}
+    values = {"action_name":"QR_LIMIT_SCENE","action_info": {"scene": {"scene_id": 1}}}
     req_data = urllib.urlencode(values)
     req = urllib2.Request(req_url, req_data)
     res = urllib2.urlopen(req)
