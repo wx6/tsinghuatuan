22bfc6e1de1c32f553f551535e4b975ee6dfdd82
李映辉 <thu2012lyh@126.com>
2014-11-26 01:03:44 +0800

    try

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -10,6 +10,7 @@ from datetime import datetime, timedelta
 from django.utils import timezone
 from userpage.safe_reverse import *
 from weixinlib.settings import WEIXIN_TOKEN
+from userpage.barcode import *
 import json
 from django.views.decorators.csrf import csrf_exempt
 from weixinlib.base_support import get_access_token
@@ -28,33 +29,6 @@ def get_timestamp():
     req = urllib2.Request(url=req_url)
     res_data = urllib2.urlopen(req)
     return res_data.read()
-
-# generate two-dimensional barcodes
-def generate_2D_barcodes(key):
-    req_url =  'https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=' + get_access_token()
-    values = "{\"action_name\":\"QR_LIMIT_SCENE\",\"action_info\": {\"scene\": {\"scene_id\": 1}}}"
-    req_data = urllib.urlencode(values)
-    print req_data
-    req = urllib2.Request(req_url, values)
-    res = urllib2.urlopen(req)
-    try:
-        res_data = res.read()
-    except:
-        return 'http://lib.tsinghua.edu.cn/dra/sites/all/themes/theme_for_note/images/bj.jpg'
-        print "exception"
-    if 'url' in res_data:
-        data = eval(res_data)
-        img_url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + data['ticket']
-        print "success"
-        return data['url']
-    else:
-        print res_data
-        return 'http://avatar.csdn.net/4/1/0/1_chenggong2dm.jpg'
-
-# Get two-dimensional barcodes from weixin server
-def get_2D_barcodes(key):
-    img_url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + str(key)
-    return img_url
 
 
 ###################### Validate ######################
--- weixinlib/base_support.py
+++ weixinlib/base_support.py
@@ -10,30 +10,28 @@ import hashlib
 #last_timestamp = int((datetime.datetime.now() + datetime.timedelta(seconds=-5)).strftime('%s'))
 
 
-# check signature as the weixin API document provided
-def check_weixin_signature(signature, timestamp, nonce):
-    #global last_timestamp
-    #timestamp_int = int(timestamp)
-    #if timestamp_int < last_timestamp - 5:
-    #    return Falsech
-    token = WEIXIN_TOKEN
-
-    tmp_list = [token, timestamp, nonce]
-    tmp_list.sort()
-    tmpstr = '%s%s%s' % tuple(tmp_list)
-    tmpstr = hashlib.sha1(tmpstr).hexdigest()
-    if tmpstr == signature:
-        #last_timestamp = timestamp_int
-        return True
+def generate_2D_barcodes(key):
+    req_url =  'https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=' + get_access_token()
+    values = {"action_name":"QR_LIMIT_SCENE","action_info": {"scene": {"scene_id": 1}}}
+    req_data = urllib.urlencode(values)
+    print req_data
+    req = urllib2.Request(req_url, req_data)
+    res = urllib2.urlopen(req)
+    try:
+        res_data = res.read()
+    except:
+        return 'http://lib.tsinghua.edu.cn/dra/sites/all/themes/theme_for_note/images/bj.jpg'
+        print "exception"
+    if 'url' in res_data:
+        data = eval(res_data)
+        img_url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + data['ticket']
+        print "success"
+        return data['url']
     else:
-        return False
+        print res_data
+        return 'http://avatar.csdn.net/4/1/0/1_chenggong2dm.jpg'
 
-
-def get_access_token():
-    url = WEIXIN_URLS['access_token']()
-    res = http_get(url)
-    rjson = json.loads(res)
-    if 'errorcode' in rjson:
-        raise res
-    return rjson['access_token']
-
+# Get two-dimensional barcodes from weixin server
+def get_2D_barcodes(key):
+    img_url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + str(key)
+    return img_url
