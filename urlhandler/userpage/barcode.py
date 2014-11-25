6b765bf3348e5c38a512eac9b22a8afae8b2f73f
李映辉 <thu2012lyh@126.com>
2014-11-25 22:26:52 +0800

    修改函数参数

--- urlhandler/userpage/barcode.py
+++ urlhandler/userpage/barcode.py
@@ -4,7 +4,7 @@ from weixinlib.settings import WEIXIN_TOKEN
 import urllib, urllib2
 
 
-def generate_2D_barcodes(request, key):
+def generate_2D_barcodes(key):
 	req_url =  'https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=' + WEIXIN_TOKEN
 	values = {"action_name":"QR_LIMIT_SCENE","action_info": {"scene": {"scene_id": key}}}
 	req_data = urllib.urlencode(values)
