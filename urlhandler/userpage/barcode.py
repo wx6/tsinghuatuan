15242901a8702ee73bfec018e28f938b598202ee
李映辉 <thu2012lyh@126.com>
2014-11-26 10:48:18 +0800

    修改value的编码格式

--- urlhandler/userpage/barcode.py
+++ urlhandler/userpage/barcode.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, Http404
 from django.template import RequestContext
 from weixinlib.settings import WEIXIN_TOKEN
 import urllib, urllib2
+import json
 from weixinlib.base_support import get_access_token
 from weixinlib.settings import WEIXIN_TOKEN
 
@@ -9,7 +10,7 @@ from weixinlib.settings import WEIXIN_TOKEN
 def generate_2D_barcodes(key):
     req_url =  'https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=' + get_access_token()
     values = {"action_name":"QR_LIMIT_SCENE","action_info": {"scene": {"scene_id": 1}}}
-    req_data = urllib.urlencode(values)
+    req_data = json.dumps(values)
     print req_data
     req = urllib2.Request(req_url, req_data)
     res = urllib2.urlopen(req)
