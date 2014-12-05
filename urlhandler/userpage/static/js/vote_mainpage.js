de9c6b98d85812446e52e7fc304d65234ef0ec87
pengyou <a623917@126.com>
2014-12-05 20:56:36 +0800

    debug

---
+++ urlhandler/userpage/static/js/vote_mainpage.js
@@ -0,0 +1,18 @@
+function commitVote() {
+    var options = {
+        dataType: 'json',
+        success: function (data) {
+        	if(data.error==null)
+        	{
+        		
+            }
+            else
+            {
+            }
+		},
+        error: function (xhr) {								
+    	}
+    };
+    $('#voteItem').ajaxSubmit(options);
+    return false;
+}
