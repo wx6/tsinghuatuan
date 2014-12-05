2e9a2430bdc92b2ec91a914fc7c794110b7e1914
Smoudy <bogeliu@126.com>
2014-12-05 16:55:44 +0800

    投票项后台

--- urlhandler/adminpage/static/js/vote_detail.js
+++ urlhandler/adminpage/static/js/vote_detail.js
@@ -245,7 +245,7 @@ function showPublishByStatus(status, linetime) {
 
 function showPubTipsByStatus(status){
     if(status < 1){
-        $('#publishBtn').tooltip({'title': '发布后不能修改“活动名称”、“活动代称”和“订票开始时间”'});
+        //$('#publishBtn').tooltip({'title': '发布后不能修改“活动名称”、“活动代称”和“订票开始时间”'});
         $('#saveBtn').tooltip({'title': '暂存后可以“继续修改”'});
     }
 }
