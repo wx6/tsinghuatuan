dd688ee0b81ef4a06b73b5decc396a6d145eacf7
thssljl2012 <frankliujunlin@163.com>
2014-12-05 19:34:50 +0800

    debug

--- weixinlib/custom_menu.py
+++ weixinlib/custom_menu.py
@@ -17,6 +17,7 @@ def get_custom_menu():
 
 def modify_custom_menu(buttons):
     #print buttons
+    print buttons
     print 'test point 3333333'
     access_token = get_access_token()
     url = WEIXIN_URLS['modify_custom_menu'](access_token)
