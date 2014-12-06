af1dd5d42a7ca5a91c4d3324f7b53a8b90fc90c3
pengyou <a623917@126.com>
2014-12-06 12:10:14 +0800

    debug

--- urlhandler/userpage/static/js/vote_mainpage.js
+++ urlhandler/userpage/static/js/vote_mainpage.js
@@ -22,34 +22,37 @@ function commitVote() {
 function onCreate(){
     var line = 3;
     var count = 0;
-    var newHtml;
+    var newHtml = "";
     var td1,td2,td3;
-    for (item in {{vote.items}})
+    for (count = 0;count < vote_items.length();count++)
     {
+        var item = vote_items[count];
        if(newHtml && (count % line) == 0 )
        {
            $("table").append(newHtml);
-           newHtml = null;
+           newHtml = "";
        }
        switch(count % line)
        {
         case 0: 
-            td1 = "<tr><td> <img src = "+{{ item.pic_url }}+" > "+" <span></span><input type ='checkbox' name="+{{item.name}}+">投票数目:{{item.vote_num}}</td>"
+            td1 = "<tr><td> <img src = "+ item.pic_url +" > "+" <span></span><input type ='checkbox' name="+item.name+">投票数目:"+item.vote_num+"</td>"
+            newHtml += td1;
             break;
         case 1:
-            td2 = "<td> <img src = "+{{ item.pic_url }}+" > "+" <span></span><input type ='checkbox' name="+{{item.name}}+">投票数目:{{item.vote_num}}</td>"
+            td2 = "<td> <img src = "+ item.pic_url +" > "+" <span></span><input type ='checkbox' name="+item.name+">投票数目:"+item.vote_num+"</td>"
+            newHtml += td2;
             break;
         case 2:
-            td3 = "<td> <img src = "+{{ item.pic_url }}+" > "+" <span></span><input type ='checkbox' name="+{{item.name}}+">投票数目:{{item.vote_num}}</td></tr>"
+            td3 = "<td> <img src = "+ item.pic_url +" > "+" <span></span><input type ='checkbox' name="+item.name+">投票数目:"+item.vote_num+"</td></tr>"
+            newHtml += td3;
             break;
         default:
             break;
        }
-
     }
     if(newHtml)
     {
         $("table").append(newHtml);
-        newHtml = null;
+        newHtml = "";
     }
 }
