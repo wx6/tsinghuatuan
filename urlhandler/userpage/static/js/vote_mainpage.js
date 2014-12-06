c53b9cf416f573c5f2aaacdd62ede0e8d506fedd
pengyou <a623917@126.com>
2014-12-06 11:34:29 +0800

    debug

--- urlhandler/userpage/static/js/vote_mainpage.js
+++ urlhandler/userpage/static/js/vote_mainpage.js
@@ -18,3 +18,38 @@ function commitVote() {
     $('#voteItem').ajaxSubmit(options);
     return false;
 }
+
+function onCreate(){
+    var line = 3;
+    var count = 0;
+    var newHtml;
+    var td1,td2,td3;
+    for (item in {{vote.items}})
+    {
+       if(newHtml && (count % line) == 0 )
+       {
+           $("table").append(newHtml);
+           newHtml = null;
+       }
+       switch(count % line)
+       {
+        case 0: 
+            td1 = "<tr><td> <img src = "+{{ item.pic_url }}+" > "+" <span></span><input type ='checkbox' name="+{{item.name}}+">投票数目:{{item.vote_num}}</td>"
+            break;
+        case 1:
+            td2 = "<td> <img src = "+{{ item.pic_url }}+" > "+" <span></span><input type ='checkbox' name="+{{item.name}}+">投票数目:{{item.vote_num}}</td>"
+            break;
+        case 2:
+            td3 = "<td> <img src = "+{{ item.pic_url }}+" > "+" <span></span><input type ='checkbox' name="+{{item.name}}+">投票数目:{{item.vote_num}}</td></tr>"
+            break;
+        default:
+            break;
+       }
+
+    }
+    if(newHtml)
+    {
+        $("table").append(newHtml);
+        newHtml = null;
+    }
+}
