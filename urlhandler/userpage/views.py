2bd48ff5b28b483a29a9255eba85df79446f6af4
thssljl2012 <frankliujunlin@163.com>
2014-12-07 22:11:34 +0800

    debug

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -99,7 +99,7 @@ def validate_through_auth(userpass):
     res_data = urllib2.urlopen(req).read()
     print 'validate process test point 5'
     # res_dict = eval(res_data)
-    print res_data
+    print type(res_data)
     print 'validate process test point 6'
     if res_data['code'] == 0:
         return 'Accepted'
