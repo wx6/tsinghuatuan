5a9a09b6c4e76c52542fadb05523e7e6280352e1
pengyou <a623917@126.com>
2014-12-07 14:23:50 +0800

    debug

--- urlhandler/userpage/static/js/vote_mainpage.js
+++ urlhandler/userpage/static/js/vote_mainpage.js
@@ -1,7 +1,7 @@
 var vote_counter;
 
 function commitVote() {
-    if(votenum <=0 || votenum > maxnum)
+    if(votenum <=0 || votenum > maxVote)
     {
         $("#hint")[0].innerHTML = "投票数目过多或者过少"
         return false
@@ -82,30 +82,6 @@ function onCreate(){
     CookieOnLoad();
 }
 
-onCreate();
-
-function addcookie(){
-    document.cookie="key=helloWorld"
-    document.cookie="key1=Hi"
-}
-
-function alertcookie () {
-    var name = escape("key")
-    name += "="
-    var allcookies = document.cookie
-    var pos = document.cookie.indexOf(name)
-    if(pos != -1){
-        var start = pos + name.length;                  //cookie值开始的位置  
-        var end = allcookies.indexOf(";",start);        //从cookie值开始的位置起搜索第一个";"的位置,即cookie值结尾的位置  
-        if (end == -1) end = allcookies.length;        //如果end值为-1说明cookie列表里只有一个cookie  
-        var value = allcookies.substring(start,end); //提取cookie的值  
-        alert(value)                           //对它解码 
-    }
-    else{
-        alert("ff")
-    }
-}
-
 function findcookie (key) {
     var name = escape(key);
     name += "=";
@@ -157,3 +133,5 @@ function CookieOnSelect(id){
         vote_counter++;
     }
 }
+
+onCreate();
