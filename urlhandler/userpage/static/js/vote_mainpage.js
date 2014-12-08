4754ab11d7d656876985726a25cc3a53d806ebd3
李映辉 <thu2012lyh@126.com>
2014-12-08 16:46:09 +0800

    投过票的页面不再显示投票选项

--- urlhandler/userpage/static/js/vote_mainpage.js
+++ urlhandler/userpage/static/js/vote_mainpage.js
@@ -39,7 +39,7 @@ function commitVote() {
 } 
 
 
-function onCreate(){
+function onCreate_unvoted(){
     var line = 3;
     var count = 0;
     var newHtml = "";
@@ -80,6 +80,56 @@ function onCreate(){
         newHtml = "";
     }
     CookieOnLoad();
+}
+
+function onCreate_voted(){
+    var line = 3;
+    var count = 0;
+    var newHtml = "";
+    var td1,td2,td3;
+    for (count = 0;count < vote_items.length;count++)
+    {
+        var item = vote_items[count];
+       if(newHtml && (count % line) == 0 )
+       {
+           $("table").append(newHtml);
+           newHtml = "";
+       }
+       switch(count % line)
+       {
+        case 0: 
+            td1 = "<tr><td><div class="+"table"+"><a href='http://wx6.igeek.asia/u/vote_item_detail/"+item.id+"'><img src = "+ item.pic_url +" ></a> "+"<p class='voteitem'>"+item.name+"</p><span>人气:"+item.vote_num+"</span></div></td>"
+            newHtml += td1;
+            break;
+        case 1:
+            td2 =  "<td><div class="+"table"+"><a href='http://wx6.igeek.asia/u/vote_item_detail/"+item.id+"'><img src = "+ item.pic_url +" ></a> "+"<p class='voteitem'>"+item.name+"</p><span>人气:"+item.vote_num+"</span></div></td>"
+            newHtml += td2;
+            break;
+        case (line-1):
+            td3 = "<td><div class="+"table"+"><a href='http://wx6.igeek.asia/u/vote_item_detail/"+item.id+"'><img src = "+ item.pic_url +" ></a> "+"<p class='voteitem'>"+item.name+"</p><span>人气:"+item.vote_num+"</span></div></td></tr>"
+            newHtml += td3;
+            break;
+        default:
+            td2 = "<td><div class="+"table"+"><a href='http://wx6.igeek.asia/u/vote_item_detail/"+item.id+"'><img src = "+ item.pic_url +" ></a> "+"<p class='voteitem;>"+item.name+"</p><span>人气:"+item.vote_num+"</span></div></td>"
+            newHtml += td2;
+            break;
+       }
+    }
+    if(count % line != 0)
+        newHtml += "</tr>";
+    if(newHtml)
+    {
+        $("table").append(newHtml);
+        newHtml = "";
+    }
+    $("button").remove();
+}
+
+function onCreate(){
+    if(voted == 0)
+        onCreate_unvoted();
+    else
+        onCreate_voted();
 }
 
 function findcookie (key) {
