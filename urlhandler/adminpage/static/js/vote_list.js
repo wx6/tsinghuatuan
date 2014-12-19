6f59c6f1ddc74861188de02bc5b0add3e35d9876
Smoudy <bogeliu@126.com>
2014-12-19 16:53:34 +0800

    投票发布

--- urlhandler/adminpage/static/js/vote_list.js
+++ urlhandler/adminpage/static/js/vote_list.js
@@ -112,10 +112,7 @@ var operationMap = {
 	},
 	'detail':function(vot){
 		return true;
-	},
-    'display':function(vot){
-        return true;
-    }
+	}
 };
 
 var tdActionMap = {
@@ -137,6 +134,12 @@ var tdActionMap = {
 	},
 	'operation_links':function(vot,key){
         var links = vot[key], result = [], i, len;
+        if (vot['display'] == 0){
+            result.push('<a href="' + vot['operations']['display'] + '" target=""><span class="glyphicon glyphicon-eye-open"></span>开启推送</a>');
+        }
+        else{
+            result.push('<a href="' + vot['operations']['display'] + '" target=""><span class="glyphicon glyphicon-eye-close"></span>取消推送</a>');
+        }
         for (i in links) {
             if (operationMap[i](vot)) {
                 result.push('<a href="' + links[i] + '" target="' + operations_target[i] + '"><span class="glyphicon glyphicon-' + operations_icon[i] + '"></span> ' + operations_name[i] + '</a>');
