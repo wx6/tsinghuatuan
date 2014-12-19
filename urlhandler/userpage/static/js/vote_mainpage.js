5790b3a313f7dc24b13edf1097e12af284dc5c1d
pengyou <a623917@126.com>
2014-12-19 20:56:06 +0800

    debug

--- urlhandler/userpage/static/js/vote_mainpage.js
+++ urlhandler/userpage/static/js/vote_mainpage.js
@@ -121,45 +121,76 @@ function addVoteNumber()
 function createBasicVoteItem()
 {
     var newHtml = "";
+    var newCb = "";
     var blankCheckBox = "<tr><td class='addcheckbox'></td><td class='addcheckbox'></td><td class='addcheckbox'></td></tr>"
     for (count = 0;count < vote_items.length;count++)
     {
        if(newHtml && (count % line) == 0 )
        {
-           $("table").append(newHtml+blankCheckBox);
+           $("table").append(newHtml+newCb+blankCheckBox);
            newHtml = "";
+           newCb = "";
        }
        newHtml += createSingalItem(count,line,vote_items);
+       newCb += createSingalItemName(count,line,vote_items);
     }
     if(count % line != 0)
+    {
         newHtml += "</tr>";
+        newCb += "</tr>";
+    }
     if(newHtml)
     {
-        $("table").append(newHtml+blankCheckBox);
+        $("table").append(newHtml+newCb+blankCheckBox);
         newHtml = "";
+        newCb = "";
     }
 }
+
+function createSingalItemName(count,line,vote_items)
+{
+    var item = vote_items[count];
+    var td = "";
+    var nameTag =  "<p class='voteitem'>"+item.name+"</p>";
+    switch(count % line)
+    {
+        case 0: 
+            td = "<tr><td>"+nameTag+"</td>";
+            break;
+        case 1:
+            td =  "<td>"+nameTag+"</td>";
+            break;
+        case (line-1):
+            td = "<td>"+nameTag+"</td></tr>";
+            break;
+        default:
+            td = "<td>"+nameTag+"</td>";
+            break;
+       }
+    return td;
+}
+
+
 
 function createSingalItem(count,line,vote_items)
 {
     var item = vote_items[count];
     var td = "";
     var imgTag =  "<a href='" + item.url + "'><img  style = "+"width:"+size+"px;height:"+size+"px></a>";
-    var nameTag =  "<p class='voteitem'>"+item.name+"</p>";
     var item = vote_items[count];
     switch(count % line)
     {
         case 0: 
-            td = "<tr><td><div class="+"table"+">"+imgTag+nameTag+"<span></span></div></td>";
+            td = "<tr><td><div class="+"table"+">"+imgTag+"<span></span></div></td>";
             break;
         case 1:
-            td =  "<td><div class="+"table"+">"+imgTag+nameTag+"<span></span></div></td>";
+            td =  "<td><div class="+"table"+">"+imgTag+"<span></span></div></td>";
             break;
         case (line-1):
-            td = "<td><div class="+"table"+">"+imgTag+nameTag+"<span></span></div></td></tr>";
+            td = "<td><div class="+"table"+">"+imgTag+"<span></span></div></td></tr>";
             break;
         default:
-            td = "<td><div class="+"table"+">"+imgTag+nameTag+"<span></span></div></td>";
+            td = "<td><div class="+"table"+">"+imgTag+"<span></span></div></td>";
             break;
        }
     return td;
