2c0f5f52e364895a850f4730edb61f724c8a4952
gdh1995 <gdh1995@qq.com>
2015-11-28 11:00:49 +0800

    app: serve as manage.py if needed

--- app.py
+++ app.py
@@ -46,3 +46,7 @@ def update_site_domain(newdomain):
     elif len(newdomain) < 4:
         return
     SITE_DOMAIN = SITE_HTTP_PROTOCOL + '://' + newdomain
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
